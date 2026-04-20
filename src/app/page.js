57176b5822783666cf3633567d3cb77ff1df6059
umayer10-AI <mdumayer10@gmail.com>
2026-04-20 06:26:21 +0600

    add

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      Hello
+      <h2 className='text-center text-3xl font-bold my-20'>Home Page</h2>
     </div>
   );
 };
---
+++ src/component/NavLink.jsx
@@ -0,0 +1,19 @@
+"use client"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const NavLink = () => {
+
+    const p = usePathname()
+
+    return (
+        <ul className="flex items-center gap-4">
+            <li><Link className={p==='/'? "btn btn-accent btn-outline" : "btn"} href="/">Home</Link></li>
+            <li><Link className={p==='/hero'? "btn btn-accent btn-outline" : "btn"} href="/hero">Hero</Link></li>
+            <li><Link className={p==='/tasks'? "btn btn-accent btn-outline" : "btn"} href="/tasks">Task</Link></li>
+        </ul>
+    );
+};
+
+export default NavLink;
